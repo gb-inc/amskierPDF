e09348d8a524d0862a29870672545afb3d515ab6
Samuel Vierling <svierling@keystone.edu>
2023-04-25 10:58:16 -0400

    dummy examples files for implementing PDFGen

---
+++ includes/gotest.php
@@ -0,0 +1,21 @@
+<?php
+
+// Generate some JSON data
+$data = array(
+  'name' => 'John Doe',
+  'age' => 42,
+  'email' => 'johndoe@example.com'
+);
+
+// Convert the JSON data to a string
+$jsonString = json_encode($data);
+
+// Call the pdfgen.exe executable with the JSON data and HTML template as arguments
+$command = "./pdfgen.exe '{$jsonString}' gotest.gohtml";
+$output = shell_exec($command);
+
+// The generated PDF should be in the $output variable
+// You can then send the PDF file to the user's browser
+header('Content-Type: application/pdf');
+header('Content-Disposition: attachment; filename="generated.pdf"');
+echo $output;
